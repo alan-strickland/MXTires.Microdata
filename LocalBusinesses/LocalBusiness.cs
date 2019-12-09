264e94945b85431441a911eab21a3b2bbb0b1604
Michael Collings <michael.collings@redtechnology.com>
2019-12-09 17:30:43 +0000

    Schema.org MXTires Library: LocalBusiness OpeningHours property uses the wrong type, and should be a list of strings

--- LocalBusinesses/LocalBusiness.cs
+++ LocalBusinesses/LocalBusiness.cs
@@ -25,7 +25,6 @@
 
 using System.Collections.Generic;
 using Newtonsoft.Json;
-using MXTires.Microdata.Intangible.Quantities;
 using MXTires.Microdata.Intangible.StructuredValues;
 
 namespace MXTires.Microdata
@@ -48,14 +47,14 @@ namespace MXTires.Microdata
         public string CurrenciesAccepted { get; set; }
 
         /// <summary>
-        /// Duration 	The opening hours for a business. Opening hours can be specified as a weekly time range, starting with days, then times per day. Multiple days can be listed with commas ',' separating each day. Day or time ranges are specified using a hyphen '-'.
+        /// Text 	The opening hours for a business. Opening hours can be specified as a weekly time range, starting with days, then times per day. Multiple days can be listed with commas ',' separating each day. Day or time ranges are specified using a hyphen '-'.
         ///- Days are specified using the following two-letter combinations: Mo, Tu, We, Th, Fr, Sa, Su.
         ///- Times are specified using 24:00 time. For example, 3pm is specified as 15:00. 
         ///- Here is an example: <time itemprop="openingHours" datetime="Tu,Th 16:00-20:00">Tuesdays and Thursdays 4-8pm</time>. 
         ///- If a business is open 7 days a week, then it can be specified as <time itemprop="openingHours" datetime="Mo-Su">Monday through Sunday, all day</time>.
         /// </summary>
         [JsonProperty("openingHours")]
-        public List<Duration> OpeningHours { get; set; }
+        public List<string> OpeningHours { get; set; }
 
         /// <summary>
         /// Text 	Cash, credit card, etc.
